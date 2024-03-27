af036f92dcdd82d918d3bedfe9ad1b72186c822f
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2024-03-27 09:50:40 -0500

    Ajuste url

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     // HOST
     //HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
     HOST_API_KEY: 'https://api.movilcelistic.com',
-    // HOST_API_KEY: 'http://localhost',
+    //HOST_API_KEY: 'http://localhost',
   },
 };
